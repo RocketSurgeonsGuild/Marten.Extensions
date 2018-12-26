fc01f760f841bc4d7d04b035dd3d14b669821d25
David Driscoll <david.driscoll@gmail.com>
2018-12-26 14:58:39 -0500

    Added extra lookups for id

--- src/Marten/ClaimsExtensions.cs
+++ src/Marten/ClaimsExtensions.cs
@@ -7,12 +7,28 @@ namespace Rocket.Surgery.Extensions.Marten
 {
     public static class ClaimsExtensions
     {
-        private static readonly string[] IdKeys = { ClaimTypes.NameIdentifier, "user_id", "sub" };
+        private static readonly string[] IdKeys = { "user_id", "sub" };
         public static string GetIdFromClaims(this IEnumerable<Claim> claims)
         {
             return IdKeys
                 .Select(key => claims.FirstOrDefault(c => c.Type == key))
                 .FirstOrDefault()?.Value;
+        }
+
+        private static bool IsIdLikeClaim(Claim claim)
+        {
+            if (claim.Type == ClaimTypes.NameIdentifier)
+            {
+                return true;
+            }
+
+            if ((claim.Type.StartsWith("http://") || claim.Type.StartsWith("https://"))
+            && (claim.Type.EndsWith("/sub") || claim.Type.EndsWith("/id") || claim.Type.EndsWith("/user_id")))
+            {
+                return true;
+            }
+
+            return IdKeys.Any(z => claim.Type == z);
         }
     }
 }
